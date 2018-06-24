13486b1db8ae3c9d69774c75c73fb3c4d2d16ea5
mithsin <hsinbluemoon@gmail.com>
2018-06-24 14:28:43 -0400

    more test

--- test/update_test.js
+++ test/update_test.js
@@ -63,7 +63,12 @@ describe('Updating records', () => {
     );
   });
 
-  it('A user can have their postcount incremented by 1', () => {
-    
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.update({ name: 'Joe'}, { $inc: { postCount: 1}})
+      .then(() => User.findOne({ name: 'Joe'}))
+        .then((user) => {
+          assert(user.postCount === 1);
+          done();
+        })
   })
 });
